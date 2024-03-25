8b036bd50c2599d4e307690569ee65b0684d003c
Jerry Thomas <me@jerrythomas.name>
2024-03-25 20:17:12 +0530

    feat: dataset includes all features of dataframe using a simplified approach

--- spec/dataset.spec.js
+++ spec/dataset.spec.js
@@ -1,18 +1,31 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, beforeEach } from 'vitest'
+import { clone } from 'ramda'
 import { dataset } from '../src/dataset'
 import { renamer } from '../src/renamer'
 import joindata from './fixtures/join'
+import scores from './fixtures/scores.json'
 
 describe('dataset', () => {
 	it('should return an object with methods', () => {
 		const ds = dataset([])
 		expect(ds).toEqual({
 			where: expect.any(Function),
+			groupBy: expect.any(Function),
+			alignBy: expect.any(Function),
+			using: expect.any(Function),
+			summarize: expect.any(Function),
+			sortBy: expect.any(Function),
+			rename: expect.any(Function),
+			delete: expect.any(Function),
+			update: expect.any(Function),
+			fillNA: expect.any(Function),
+			rollup: expect.any(Function),
+			apply: expect.any(Function),
+			drop: expect.any(Function),
 			select: expect.any(Function),
 			union: expect.any(Function),
+			minus: expect.any(Function),
 			intersect: expect.any(Function),
-			difference: expect.any(Function),
-			rename: expect.any(Function),
 			innerJoin: expect.any(Function),
 			leftJoin: expect.any(Function),
 			rightJoin: expect.any(Function),
@@ -73,27 +86,208 @@ describe('dataset', () => {
 				.select()
 			expect(result).toEqual([{ a: 2 }])
 		})
-		it('should perform difference operation', () => {
+		it('should perform minus operation', () => {
 			const result = dataset([{ a: 1 }, { a: 2 }])
-				.difference([{ a: 2 }, { a: 3 }])
+				.minus([{ a: 2 }, { a: 3 }])
 				.select()
 			expect(result).toEqual([{ a: 1 }])
 		})
 	})
 
-	describe('rename', () => {
-		it('should perform rename using function', () => {
-			const { renameObject } = renamer({ prefix: 'x', keys: ['a'] }).get()
-			const result = dataset([{ a: 1 }, { a: 2 }])
-				.rename(renameObject)
-				.select()
-			expect(result).toEqual([{ x_a: 1 }, { x_a: 2 }])
+	describe('transform', () => {
+		describe('rename', () => {
+			it('should perform rename using function', () => {
+				const { renameObject } = renamer({ prefix: 'x', keys: ['a'] }).get()
+				const result = dataset([{ a: 1 }, { a: 2 }])
+					.rename(renameObject)
+					.select()
+				expect(result).toEqual([{ x_a: 1 }, { x_a: 2 }])
+			})
+			it('should perform rename using a key map', () => {
+				const result = dataset([
+					{ b: 'X', a: 1, c: 3 },
+					{ a: 2, b: 'Y' }
+				])
+					.rename({ a: 'b' })
+					.select()
+				expect(result).toEqual([{ b: 1, c: 3 }, { b: 2 }])
+			})
 		})
-		it('should perform rename using a key map', () => {
-			const result = dataset([{ a: 1, c: 3 }, { a: 2 }])
-				.rename({ a: 'b' })
-				.select()
-			expect(result).toEqual([{ b: 1, c: 3 }, { b: 2 }])
+		describe('drop', () => {
+			it('should drop column', () => {
+				const data = [{ a: 1, b: 2 }]
+
+				const result = dataset(data).drop('a')
+				expect(result.select()).toEqual([{ b: 2 }])
+				expect(result.select()).not.toBe(data)
+			})
+
+			it('should drop multiple columns', () => {
+				const data = [{ a: 1, b: 2, c: 3 }]
+				const result = dataset(data).drop('a', 'c')
+				expect(result.select()).toEqual([{ b: 2 }])
+				expect(result.select()).not.toBe(data)
+			})
+		})
+		describe('update', () => {
+			let data
+
+			beforeEach(() => {
+				data = [
+					{ a: 1, b: 2 },
+					{ a: 2, b: 3 },
+					{ a: 3, b: 4 }
+				]
+			})
+
+			it('should throw error when input is not object', () => {
+				expect(() => dataset(data).update('a')).toThrow('Value must be an object or function')
+			})
+			it('should update a record', () => {
+				const result = dataset(data).update({ a: 'x' })
+				expect(result.select()).toEqual([
+					{ a: 'x', b: 2 },
+					{ a: 'x', b: 3 },
+					{ a: 'x', b: 4 }
+				])
+				expect(result.select()).toEqual(data)
+			})
+
+			it('should update a record with multiple attributes', () => {
+				const result = dataset(data).update({ c: 2, y: 4 })
+				expect(result.select()).toEqual([
+					{ a: 1, b: 2, c: 2, y: 4 },
+					{ a: 2, b: 3, c: 2, y: 4 },
+					{ a: 3, b: 4, c: 2, y: 4 }
+				])
+				expect(result.select()).toEqual(data)
+			})
+
+			it('should update selected rows only', () => {
+				const matcher = (row) => row.a === 2
+				const result = dataset(data).where(matcher).update({ a: 9 })
+				expect(result.select()).toEqual([
+					{ a: 1, b: 2 },
+					{ a: 9, b: 3 },
+					{ a: 3, b: 4 }
+				])
+				expect(result.select()).toEqual(data)
+			})
+
+			it('should update using a function', () => {
+				const result = dataset(data).update((row) => (row.a = row.a * 2))
+				expect(result.select()).toEqual([
+					{ a: 2, b: 2 },
+					{ a: 4, b: 3 },
+					{ a: 6, b: 4 }
+				])
+			})
+		})
+		describe('delete', () => {
+			let data
+
+			beforeEach(() => {
+				data = [
+					{ a: 1, b: 2 },
+					{ a: 2, b: 3 },
+					{ a: 3, b: 4 }
+				]
+			})
+
+			it('should delete all records', () => {
+				const result = dataset(data).delete()
+				expect(result.select()).toEqual([])
+				expect(result.select()).toEqual(data)
+			})
+
+			it('should delete selected rows only', () => {
+				const matcher = (row) => row.a === 2
+				const result = dataset(data).where(matcher).delete()
+				expect(result.select()).toEqual([
+					{ a: 1, b: 2 },
+					{ a: 3, b: 4 }
+				])
+				expect(result.select()).toEqual(data)
+			})
+		})
+
+		describe('fillNA', () => {
+			it('should fill NA values', () => {
+				const data = [
+					{ a: 1, b: 2 },
+					{ a: 2, b: null },
+					{ a: null, b: 4 }
+				]
+				const result = dataset(data).fillNA({ b: 0 })
+				expect(result.select()).toEqual([
+					{ a: 1, b: 2 },
+					{ a: 2, b: 0 },
+					{ a: null, b: 4 }
+				])
+			})
+		})
+		describe('sort', () => {
+			let data
+			beforeEach(() => {
+				data = clone(scores)
+			})
+			it('should sort the data frame', () => {
+				const result = dataset(data).sortBy('name')
+
+				expect(result.select().map(({ name }) => name)).toEqual([
+					'Heeyong Park',
+					'Karine Abrahim',
+					'Markus Ertelt',
+					'Matias Chavez',
+					'Myon Tuk Han',
+					'Omar Zamitiz',
+					'Ricardo de Oliviera',
+					'Shaun Provost',
+					'Shinobi Poli',
+					'Simon Brunner',
+					'Takehide Sato',
+					'Toyohiko Kubota'
+				])
+				expect(result.select()).toEqual(data)
+			})
+			it('should sort the data frame in descending order', () => {
+				const result = dataset(data).sortBy(['name', false])
+
+				expect(result.select('name')).toEqual([
+					{ name: 'Toyohiko Kubota' },
+					{ name: 'Takehide Sato' },
+					{ name: 'Simon Brunner' },
+					{ name: 'Shinobi Poli' },
+					{ name: 'Shaun Provost' },
+					{ name: 'Ricardo de Oliviera' },
+					{ name: 'Omar Zamitiz' },
+					{ name: 'Myon Tuk Han' },
+					{ name: 'Matias Chavez' },
+					{ name: 'Markus Ertelt' },
+					{ name: 'Karine Abrahim' },
+					{ name: 'Heeyong Park' }
+				])
+				expect(result.select()).toEqual(data)
+			})
+			it('should sort by multiple columns', () => {
+				const result = dataset(data).sortBy('country', 'name')
+
+				expect(result.select('country', 'name')).toEqual([
+					{ country: 'Brazil', name: 'Karine Abrahim' },
+					{ country: 'Brazil', name: 'Ricardo de Oliviera' },
+					{ country: 'Germany', name: 'Markus Ertelt' },
+					{ country: 'Germany', name: 'Simon Brunner' },
+					{ country: 'Japan', name: 'Takehide Sato' },
+					{ country: 'Japan', name: 'Toyohiko Kubota' },
+					{ country: 'Mexico', name: 'Matias Chavez' },
+					{ country: 'Mexico', name: 'Omar Zamitiz' },
+					{ country: 'South Korea', name: 'Heeyong Park' },
+					{ country: 'South Korea', name: 'Myon Tuk Han' },
+					{ country: 'United States', name: 'Shaun Provost' },
+					{ country: 'United States', name: 'Shinobi Poli' }
+				])
+				expect(result.select()).toEqual(data)
+			})
 		})
 	})
 
--- src/dataset.js
+++ src/dataset.js
@@ -1,28 +1,41 @@
-import { equals, identity, pick } from 'ramda'
-
+import { equals, identity, pick, omit, clone } from 'ramda'
+import { descending } from 'd3-array'
+import { defaultConfig } from './constants'
+import { deriveSortableColumn } from './infer'
+import { groupDataByKeys, fillAlignedData, getAlignGenerator, aggregateData } from './rollup'
 /**
  * Dataset is a collection of data with a set of operations that can be performed on it.
  * @param {Array} data - The data to be stored in the dataset.
+ * @param {Object} options - The configuration options for the dataset.
  * @returns {Object} - An object with a set of operations that can be performed on the dataset.
  */
-
-export function dataset(data) {
-	let where_condition = null
+// eslint-disable-next-line max-lines-per-function
+export function dataset(data, options = {}) {
+	const config = { ...clone(defaultConfig), ...options }
 
 	const actions = {
-		where: (condition) => {
-			where_condition = condition
-			return actions
-		},
-		select: (...cols) => {
-			const result = where_condition ? data.filter(where_condition) : data
-			if (cols.length > 0) return result.map(pick(cols))
-			return result
-		},
+		// configuration
+		where: (condition) => dataset(data, where(config, condition)),
+		groupBy: (...fields) => dataset(data, groupBy(config, ...fields)),
+		alignBy: (...fields) => dataset(data, alignBy(config, ...fields)),
+		using: (template) => dataset(data, using(config, template)),
+		summarize: (from, using) => dataset(data, summarize(config, from, using)),
+		// modifiers
+		sortBy: (...fields) => dataset(sortDataBy(data, ...fields)),
 		rename: (how) => dataset(renameKeys(data, how)),
+		drop: (...fields) => dataset(dropKeys(data, ...fields)),
+		delete: () => dataset(deleteRows(data, config.filter || identity)),
+		update: (value) => dataset(updateRows(data, config.filter || identity, value)),
+		apply: (callback) => dataset(data.map(callback)),
+		fillNA: (value) => dataset(fillNA(data, value)),
+		rollup: () => dataset(rollup(data, config)),
+		select: (...cols) => selectKeys(data, config, ...cols),
+
+		// set operations
 		union: (other) => dataset(data.concat(other)),
+		minus: (other) => dataset(data.filter((d) => !other.find((x) => equals(x, d)))),
 		intersect: (other) => dataset(data.filter((d) => other.find((x) => equals(x, d)))),
-		difference: (other) => dataset(data.filter((d) => !other.find((x) => equals(x, d)))),
+		// joins
 		innerJoin: (other, condition) => dataset(innerJoin(data, other.select(), condition)),
 		leftJoin: (other, condition) => dataset(leftJoin(data, other.select(), condition)),
 		rightJoin: (other, condition) => dataset(rightJoin(data, other.select(), condition)),
@@ -34,6 +47,73 @@ export function dataset(data) {
 	}
 
 	return actions
+}
+
+/**
+ * Adds a filter to the dataset using the provided condition. This filter is applied
+ * in subsequent operations like select, delete, and update.
+ *
+ * @param {Object} config      - The configuration object to be updated.
+ * @param {Function} condition - The condition function to apply.
+ * @returns {Object}           - The updated configuration
+ */
+function where(config, condition) {
+	config.filter = condition
+	return config
+}
+/**
+ * Groups the dataset by the specified columns.
+ * @param {Object} config      - The configuration object to be updated.
+ * @param {Object} actions     - The actions object for method chaining.
+ * @param {...string} fields   - The columns to group by.
+ * @returns {Object}           - The updated configurations.
+ */
+function groupBy(config, ...fields) {
+	config.group_by = fields
+	return config
+}
+
+/**
+ * Aligns the columns of the dataset using the provided fields.
+ *
+ * @param {Object} config      - The configuration object to be updated.
+ * @param {...string} fields   - The fields to align.
+ * @returns {Object}           - The updated configuration.
+ */
+function alignBy(config, ...fields) {
+	config.align_by = fields
+	return config
+}
+/**
+ * Sets the template for adding empty rows in the dataset.
+ * @param {Object} config      - The configuration object to be updated.
+ * @param {Object} template    - The template to use for adding empty rows.
+ * @returns {Object}           - The updated configuration.
+ */
+function using(config, template) {
+	config.template = template
+	return config
+}
+
+/**
+ * Adds a summary field to the dataset.
+ *
+ * @param {Object} config                        - The configuration object to be updated.
+ * @param {string|Array<string>|Function}  from  - The field or function to fetch data for summary
+ * @param {string|Object<string:Function>} using - The target field & formula to use for summarizing.
+ * @returns {Object}                             - The updated configuration.
+ */
+function summarize(config, from, using) {
+	const mapper = typeof from === 'function' ? from : Array.isArray(from) ? pick(from) : pick([from])
+	const reducers = []
+
+	if (typeof using === 'string') reducers.push({ field: using, formula: identity })
+	if (typeof using === 'object')
+		Object.entries(using).forEach(([field, formula]) => reducers.push({ field, formula }))
+
+	config.summaries.push({ mapper, reducers })
+
+	return config
 }
 
 /**
@@ -183,11 +263,174 @@ function nestedJoin(first, second, condition, key = 'children') {
 	return result
 }
 
+/**
+ * Renames the keys of the data based on the provided mapping, or function
+ * that returns the new key. If the new key already exists, the value is overwritten.
+ *
+ * @param {Array<Object>}   data - The data to rename the keys of.
+ * @param {Object|Function} how  - The mapping of old keys to new keys, or a function that performs renaming.
+ * returns Array<Object>         - The updated data with renamed keys.
+ */
 function renameKeys(data, how) {
 	const rename = typeof how === 'function' ? how : renameKeysUsingMap(how)
 	return data.map(rename)
 }
 
+/**
+ * Drops the specified keys from the data.
+ *
+ * @param {Array<Object>} data - The data to drop keys from.
+ * @param {Array<String>} keys - The keys to drop.
+ * @returns {Array<Object>}    - The updated data with the specified keys dropped.
+ */
+function dropKeys(data, ...keys) {
+	return data.map((row) => omit(keys, row))
+}
+
+/**
+ * Deletes rows from the data based on the condition
+ *
+ * @param {Array<Object>} data      - The data to delete rows from.
+ * @param {Function}      condition - The condition to delete rows on.
+ * @returns {Array<Object>}         - The updated data with rows deleted based on the condition.
+ */
+function deleteRows(data, condition) {
+	for (let i = data.length - 1; i >= 0; i--) {
+		if (condition(data[i])) data.splice(i, 1)
+	}
+	return data
+}
+
+/**
+ * Updates rows in the data based on the condition
+ *
+ * @param {Array<Object>}   data      - The data to update rows in.
+ * @param {Function}        filter    - The condition to update rows on.
+ * @param {Object|Function} value     - The values to update the rows with.
+ * @returns {Array<Object>}           - The updated data with rows updated based on the condition.
+ */
+function updateRows(data, filter, value) {
+	if (typeof value !== 'function' && typeof value !== 'object') {
+		throw new Error('Value must be an object or function')
+	}
+	const updater =
+		typeof value === 'function'
+			? value
+			: (row) => Object.entries(value).forEach(([k, v]) => (row[k] = v))
+	data.forEach((row) => {
+		if (filter(row)) updater(row)
+	})
+
+	return data
+}
+
+/**
+ * Fills missing values in the data based on the provided value.
+ *
+ * @param {Array<Object>} data  - The data to fill missing values in.
+ * @param {Object}        value - The value to fill missing values with.
+ * @returns {Array<Object>}     - The updated data with missing values filled.
+ */
+function fillNA(data, value) {
+	const fill = (row) => {
+		Object.entries(value).forEach(([k, v]) => {
+			if (row[k] === undefined || row[k] === null) row[k] = v
+		})
+	}
+	data.forEach(fill)
+	return data
+}
+
+/**
+ * Returns a function that renames the keys of a single row based on the provided mapping.
+ *
+ * @param {Object} lookup - The mapping of old keys to new keys.
+ * @returns {Function}    - The function that renames the keys of a single row.
+ */
 function renameKeysUsingMap(lookup) {
-	return (row) => Object.entries(row).reduce((acc, [k, v]) => ({ ...acc, [lookup[k] || k]: v }), {})
+	return (row) =>
+		Object.entries(row)
+			.sort((a, b) => descending(a[0], b[0]))
+			.reduce((acc, [k, v]) => ({ ...acc, [lookup[k] || k]: v }), {})
+}
+
+/**
+ * Sorts data by the specified columns.
+ *
+ * @param {Array<Object>}                    data   - The data to sort.
+ * @param {import('./types').SortableColumn} fields - The columns to sort by.
+ * @returns {Array<Object>}                           The sorted data
+ */
+function sortDataBy(data, ...fields) {
+	const sorters = fields.map(deriveSortableColumn)
+
+	data.sort((a, b) => {
+		let result = 0
+		for (let i = 0; i < sorters.length && result === 0; i++) {
+			const { name, sorter } = sorters[i]
+			result = sorter(a[name], b[name])
+		}
+		return result
+	})
+	return data
+}
+
+/**
+ * Selects the specified columns from the dataset.
+ *
+ * @param {Array<Object>} data          - The data to select from.
+ * @param {Object}        config        - The configuration object.
+ * @param {Function}      config.filter - The filter function to apply to the data.
+ * @param {Array<String>} cols          - The columns to select.
+ * @returns {Array<Object>} The selected data.
+ */
+function selectKeys(data, config, ...cols) {
+	const result = config.filter ? data.filter(config.filter) : data
+	if (cols.length > 0) return result.map(pick(cols))
+	return result
+}
+
+/**
+ * Summarizes the dataset by the specified columns.
+ * @param {import('./types').dataset} df - The dataset object to summarize.
+ * @param {Array} summaries                  - The columns to summarize.
+ *
+ * @returns {import('./types').dataset}  The summarized dataset object.
+ */
+function rollup(data, config) {
+	if (config.group_by.length === 0 && config.summaries.length === 0) {
+		throw new Error(
+			'Use groupBy to specify the columns to group by or use summarize to add aggregators.'
+		)
+	}
+
+	const summaries = clone(config.summaries)
+	const hasAlignBy = config.align_by.length > 0
+	if (summaries.length === 0) {
+		summaries.push(defaultAggregator(config))
+	}
+
+	let alignedData = groupDataByKeys(data, config.group_by, summaries)
+
+	if (hasAlignBy) {
+		const fillRows = getAlignGenerator(data, config)
+		alignedData = fillAlignedData(alignedData, config, fillRows)
+	}
+
+	const aggregatedData = aggregateData(alignedData, summaries)
+	// const newMetadata = buildMetadata(aggregatedData, df.metadata, df.config.group_by, summaries)
+
+	return aggregatedData
+}
+
+/**
+ * Returns the default aggregator which rolls up the columns other than the group_by columns into a single object.
+ *
+ * @param {import('./types').Metadata} metadata - The metadata for the columns to be aggregated.
+ * @param {Object} config                       - The configuration used to build the aggregator.
+ *
+ * @returns {Object} An object containing the default aggregator for the specified metadata and configuration.
+ */
+export function defaultAggregator(config) {
+	return { mapper: omit(config.group_by), reducers: [{ field: 'children', formula: identity }] }
 }
